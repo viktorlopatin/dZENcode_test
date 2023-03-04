8cdba11d5f1db8e497cc8075bf19a1f669979040
viktor <fdfnfhfyr13@gmail.com>
2023-03-04 13:26:50 +0200

    added docker

--- djsite/dZENcode_test/dZENcode_test/settings.py
+++ djsite/dZENcode_test/dZENcode_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--a)jyr_in*lds)2du$(%++rh5o$jv$o$p5inxtikv#7_udos3r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
